9828cd793b781e709bf8040bbb0b650e097571a9
muralivijay <muralidharan9845@gmail.com>
2023-10-15 19:38:14 +0000

    drivers:extcon: fix kernel panic.
    [    0.988717] Hardware name: Qualcomm Technologies, Inc. SPES KHAJE IDP nopmi (DT)
    [    0.988723] pstate: 60000085 (nZCv daIf -PAN -UAO)
    [    0.988750] pc : devm_kmalloc+0x98/0xc4
    [    0.988754] lr : devm_kmalloc+0x70/0xc4
    [    0.988758] sp : ffffff80080639a0
    [    0.988763] x29: ffffff80080639a0 x28: ffffff972635bad0
    [    0.988768] x27: ffffff972635ba84 x26: 0000000000000124
    [    0.988774] x25: 00000000000001e8 x24: ffffff9727648547
    [    0.988779] x23: ffffff972752a819 x22: ffffff9727593356
    [    0.988783] x21: ffffffd505123b30 x20: ffffffd505132b00
    [    0.988788] x19: ffffffd505132b80 x18: ffffff800805d098
    [    0.988793] x17: ffffffd50512f298 x16: 0000000000000010
    [    0.988797] x15: ffffff9727197bf4 x14: ffffff97273ebd48
    [    0.988802] x13: 000000000000003f x12: ffffffd5f7dd9300
    [    0.988806] x11: f8e11adeffb07c00 x10: f8e11adeffb07c00
    [    0.988811] x9 : 0000000000000000 x8 : ffffffd505123b38
    [    0.988816] x7 : 0000000000000000 x6 : 000000000000003f
    [    0.988820] x5 : 0000000000000040 x4 : 0000000000000000
    [    0.988825] x3 : ffffff8008063918 x2 : ffffff9725c94758
    [    0.988830] x1 : 0000000000000000 x0 : ffffffd505123b30
    [    0.988837] Call trace:
    [    0.988844]  devm_kmalloc+0x98/0xc4
    [    0.988861]  extcon_dev_register+0x460/0x6b4
    [    0.988866]  devm_extcon_dev_register+0x48/0x8c
    [    0.988880]  msm_eud_probe+0x8c/0x3bc
    [    0.988885]  platform_drv_probe+0x80/0xbc
    [    0.988891]  really_probe+0x27c/0x300
    [    0.988895]  driver_probe_device+0x64/0x100
    [    0.988900]  __driver_attach+0xd0/0x10c
    [    0.988909]  bus_for_each_dev+0x7c/0xc8
    [    0.988913]  driver_attach+0x24/0x30
    [    0.988918]  bus_add_driver+0x124/0x204
    [    0.988922]  driver_register+0x78/0x110
    [    0.988926]  __platform_driver_register+0x44/0x50
    [    0.988941]  msm_eud_init+0x30/0x8c
    [    0.988953]  do_one_initcall+0x130/0x290
    [    0.988965]  do_initcall_level+0x144/0x170
    [    0.988970]  do_basic_setup+0x34/0x50
    [    0.988974]  kernel_init_freeable+0xc8/0x14c
    [    0.988982]  kernel_init+0x18/0x294
    [    0.988989]  ret_from_fork+0x10/0x18
    [    0.988996] Code: aa1503e0 f9019674 91020293 a9002688 (f9000134)
    [    0.989008] ---[ end trace 34fb0c98d3ca56f6 ]---
    [    0.989567] note: swapper/0[1] exited with preempt_count 1
    [    0.989598] Kernel panic - not syncing: Attempted to kill init! exitcode=0x0000000b
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>
    Signed-off-by: warudooooo <warudo.2k23@gmail.com>

--- drivers/extcon/extcon.c
+++ drivers/extcon/extcon.c
@@ -1321,8 +1321,7 @@ int extcon_dev_register(struct extcon_dev *edev)
 		}
 	}
 
-	edev->bnh = devm_kzalloc(&edev->dev,
-			sizeof(*edev->bnh) * edev->max_supported, GFP_KERNEL);
+	edev->bnh = kzalloc(sizeof(*edev->bnh) * edev->max_supported, GFP_KERNEL);
 	if (!edev->bnh) {
 		ret = -ENOMEM;
 		goto err_dev;
@@ -1341,7 +1340,7 @@ int extcon_dev_register(struct extcon_dev *edev)
 	ret = device_register(&edev->dev);
 	if (ret) {
 		put_device(&edev->dev);
-		goto err_dev;
+		goto err_reg;
 	}
 
 	mutex_lock(&extcon_dev_list_lock);
@@ -1350,6 +1349,8 @@ int extcon_dev_register(struct extcon_dev *edev)
 
 	return 0;
 
+err_reg:
+	kfree(edev->bnh);
 err_dev:
 	if (edev->max_supported)
 		kfree(edev->nh);
@@ -1416,6 +1417,7 @@ void extcon_dev_unregister(struct extcon_dev *edev)
 		kfree(edev->cables);
 		kfree(edev->nh);
 	}
+	kfree(edev->bnh);
 
 	put_device(&edev->dev);
 }
